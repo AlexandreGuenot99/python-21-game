a66e8c59864d368f8bce2ce24a81968066d6b48c
Alexandre Guenot <alexandreguenot@Host-005.lan>
2025-07-27 00:24:08 +0200

    finished game

--- main.py
+++ main.py
@@ -25,10 +25,21 @@ def choose_amount():
         amount = input('How many numbers do you wish to enter?\n')
         try:
             amount = int(amount)
-            if 1 <= amount <= 3 :
-                return amount
-            else :
-                print('Type a value between 1 and 3')
+            if len(game_board) == 18:
+                if 1 <= amount <= 2:
+                    return amount
+                else:
+                    print('Type a value between 1 and 2')
+            elif len(game_board) == 19:
+                if amount ==1 :
+                    return amount
+                else:
+                    print('Type a value between 1 and 2')
+            else:
+                if 1 <= amount <= 3 :
+                    return amount
+                else :
+                    print('Type a value between 1 and 3')
         except ValueError:
             print('Type a numeral value')
 
@@ -50,36 +61,46 @@ def choose_values(amount):
     return input_values
 
 def check_consecutive(board):
+    is_consecutive_counter = 0
     if len(board) ==1:
-        is_consecutive = True
+        is_consecutive_counter = 0
     else:
         for i in range(len(board)-1):
             if board[i+1] - board[i] !=1:
-                is_consecutive = False
-            else:
-                is_consecutive = True
+                is_consecutive_counter += 1
            
-    return is_consecutive     
+    return is_consecutive_counter     
 
 def game():
     is_playing = choose_position()
-    while True:
+    while len(game_board) < 20 :
         if is_playing == True:
-            print('Your turn')
+            print('\nYour turn\n')
             amount = choose_amount()
             input_values = choose_values(amount) 
             consecutive = check_consecutive(input_values)
-            if consecutive:
-                for i in range(len(input_values)):
-                    game_board.append(input_values[i])
+            if consecutive == 0:
+                if not game_board:
+                    for i in range(len(input_values)):
+                        game_board.append(input_values[i])
+                else:
+                    if input_values[0] != game_board[-1]+1:
+                        print('Your values are not following the sequence. You lost !')
+                        print(f'Expected : {game_board[-1]+1} as first value')
+                        break
+                    for i in range(len(input_values)):
+                        game_board.append(input_values[i])
             else:
                 print('Your values are not crescent consecutives. You lost !')
-                break
+                break         
             is_playing = False
         else:
-            print('Computer turn')
-            amount = random.randint(1,3)
-            print(f'Computer add {amount} values')
+            if len(game_board) == 18:
+                amount = random.randint(1,2)
+            elif len(game_board) == 19:
+                amount = 1
+            else:
+                amount = random.randint(1,3)
             cpu_values=[]
             if not game_board:
                 for i in range(amount):
@@ -90,6 +111,11 @@ def game():
                     game_board.append(cpu_values[i])
             is_playing = True
             print(f'Order of input after Computer turn is :\n {game_board}')
+    if is_playing == True:
+        print('The player ends on 21. Player lost !')
+    elif is_playing == False:
+        print('The computer ends on 21. Player win !')
+
     
 game()
 
